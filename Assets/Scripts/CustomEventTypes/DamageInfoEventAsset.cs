df42617a0b9d2df11c344452ae0bc2c4d7a521e2
pg25wyatt <pg25wyatt@vfs.com>
2024-02-26 12:15:24 -0800

    commit assets

--- Assets/Scripts/CustomEventTypes/DamageInfoEventAsset.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace GameEvents
-{
-    [CreateAssetMenu(menuName = "Events/DamageInfo Event Asset")]
-    public class DamageInfoEventAsset : GameEventAsset<DamageInfo> {}
-}
